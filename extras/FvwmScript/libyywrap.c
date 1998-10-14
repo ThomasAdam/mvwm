c456eb8633e8fe4f0924f7f3904dc62ba58e1196
tibbs <tibbs>
1998-10-14 00:03:21 +0000

    Initial revision

---
+++ extras/FvwmScript/libyywrap.c
@@ -0,0 +1,8 @@
+/* libyywrap - flex run-time support library "yywrap" function */
+
+/* $Header: /home/cvs/fvwm/fvwm/extras/FvwmScript/Attic/libyywrap.c,v 1.1 1998/10/14 00:03:28 tibbs Exp $ */
+
+int yywrap()
+	{
+	return 1;
+	}
---
+++ sample.fvwmrc/BradyMontz/src/xpaste-1.1/patchlevel.h
@@ -0,0 +1,3 @@
+#ifndef lint
+static char patchlevel[] = "xpaste Version 1.1 $Id: patchlevel.h,v 1.1 1998/10/14 00:03:25 tibbs Exp $";
+#endif
