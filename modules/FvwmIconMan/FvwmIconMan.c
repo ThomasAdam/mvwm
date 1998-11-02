48de3d568aeb2eb13f2e29aca95d7037b490fefb
steve <steve>
1998-11-02 15:38:34 +0000

    Removed an unnecessary line in fvwm/Makefile.am, #included config.h in FvwmIconMan.h.

--- modules/FvwmIconMan/FvwmIconMan.c
+++ modules/FvwmIconMan/FvwmIconMan.c
@@ -13,10 +13,10 @@
 #endif
 
 static int fd_width;
-static char *VERSION = "1.3";
+static char *IM_VERSION = "1.3";
 
 static char const rcsid[] =
-  "$Id: FvwmIconMan.c,v 1.2 1998/10/30 21:19:44 domivogt Exp $";
+  "$Id: FvwmIconMan.c,v 1.3 1998/11/02 15:38:35 steve Exp $";
 
 char *copy_string (char **target, char *src)
 {
@@ -192,7 +192,7 @@ int main (int argc, char **argv)
 
   if((argc != 6) && (argc != 7)) {
     fprintf(stderr,"%s Version %s should only be executed by fvwm!\n",Module,
-      VERSION);
+      IM_VERSION);
     ShutMeDown (1);
   }
   if (argc == 7 && !strcasecmp (argv[6], "Transient"))
