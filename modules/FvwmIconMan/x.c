8f06a93138738ec94f62aa8c2bde3a991ad2f299
steve <steve>
1999-03-03 01:25:18 +0000

    Many fixes to remove warnings when using SGI's compiler.  Mostly the changes involved using the correct types (unsigned in in X calls), and using 'extern' to avoid multiple definitions of variables.
    I didn't manage to remove all multiple definitions.  Would someone who
    understands X11 please check whether the various definitions of
    _XA_WM_PROTOCOLS should be shared (hence one instance extern'd) or
    private (hence both instances static'd).  Thanks.

--- modules/FvwmIconMan/x.c
+++ modules/FvwmIconMan/x.c
@@ -5,7 +5,7 @@
 #include "xmanager.h"
 
 static char const rcsid[] =
-  "$Id: x.c,v 1.9 1999/01/10 20:59:59 steve Exp $";
+  "$Id: x.c,v 1.10 1999/03/03 01:25:23 steve Exp $";
 
 #define GRAB_EVENTS (ButtonPressMask|ButtonReleaseMask|ButtonMotionMask|EnterWindowMask|LeaveWindowMask)
 
@@ -121,7 +121,7 @@ static void handle_buttonevent (XEvent *theEvent, WinManager *man)
 Window find_frame_window (Window win, int *off_x, int *off_y)
 {
   Window root, parent, *junkw;
-  int junki;
+  unsigned int junki;
   XWindowAttributes attr;
 
   ConsoleDebug (X11, "In find_frame_window: 0x%x\n", win);
@@ -462,7 +462,7 @@ void read_all_reparent_events (WinManager *man, int block)
 void X_init_manager (int man_id)
 {
   WinManager *man;
-  int width, height;
+  unsigned int width, height;
   int i, x, y, geometry_mask;
   ConsoleDebug (X11, "In X_init_manager\n");
 
@@ -618,10 +618,11 @@ void X_init_manager (int man_id)
   if (globals.transient) {
     Window dummyroot, dummychild;
     int junk;
+    unsigned int ujunk;
 
     XQueryPointer(theDisplay, theRoot, &dummyroot, &dummychild,
 		  &man->geometry.x,
-		  &man->geometry.y, &junk, &junk, &junk);
+		  &man->geometry.y, &junk, &junk, &ujunk);
     man->geometry.dir |= GROW_DOWN | GROW_RIGHT;
     man->sizehints_flags |= USPosition;
   }
