65a179f45b0a8407bc9b4a96872486ad5a80b2fd
domivogt <domivogt>
1999-03-07 02:40:33 +0000

    * removed patches that changed names of Send_windowList, Send_Config_Info   and set_mask
    * fixed sort order bug (CygWin) by forcing function table names to be
      upper case
    
    * fixed coredump when a menu/function tries to delete itself
    
    * merged functions.c and complex.c
    
    * moved the code for function handling from menus.c to functions.c (this is a
      bit more code but saves a lot of bytes for each item of a function).

--- modules/FvwmIconMan/FvwmIconMan.c
+++ modules/FvwmIconMan/FvwmIconMan.c
@@ -21,7 +21,7 @@ static volatile sig_atomic_t isTerminated = False;
 static char *IM_VERSION = "1.3";
 
 static char const rcsid[] =
-  "$Id: FvwmIconMan.c,v 1.15 1999/03/05 13:58:47 domivogt Exp $";
+  "$Id: FvwmIconMan.c,v 1.16 1999/03/07 02:40:48 domivogt Exp $";
 
 
 static RETSIGTYPE TerminateHandler(int);
@@ -255,7 +255,7 @@ int main (int argc, char **argv)
 #endif
 		 M_STRING);
 
-  SendInfo (Fvwm_fd, "SendWindowList", 0);
+  SendInfo (Fvwm_fd, "Send_WindowList", 0);
 
   main_loop();
 
