a730d6cdc78174dc338c531ffbc3bcdf43edc906
steve <steve>
1999-03-10 03:08:48 +0000

    Have patience -- nearly done.

--- extras/FvwmScript/libyywrap.c
+++
@@ -1,8 +0,0 @@
-/* libyywrap - flex run-time support library "yywrap" function */
-
-/* $Header: /home/cvs/fvwm/fvwm/extras/FvwmScript/Attic/libyywrap.c,v 1.2 1998/10/30 21:19:32 domivogt Exp $ */
-
-int yywrap()
-	{
-	return 1;
-	}
