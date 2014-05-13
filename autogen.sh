6631bc0beecb8baa4e500add6f0115b0ac8c9360
Thomas Adam <thomas@xteddy.org>
2014-05-13 11:54:22 +0100

    Automake:  Use foregin definition
    Rather than litter the top-level directory with automake spew, define
    --foreign and --copy and use it.

--- autogen.sh
+++ autogen.sh
@@ -3,8 +3,9 @@
 # reconfigure CVS source code
 # must be called from main fvwm directory
 set -x
+mkdir -p etc
 aclocal || exit 1
 autoheader || exit 2
-automake --add-missing || exit 3
+automake --add-missing --force-missing --copy --foreign || exit 3
 autoreconf || exit 4
 ./configure ${1+"$@"} || exit 5
